26df6b84986e8620e0c7f501970f123341e9d1b3
= <1632431541@qq.com>
2025-04-26 15:42:06 +0800

    增加多线程处理摄像头，降低延迟

--- video.cpp
+++ video.cpp
@@ -1,43 +1,121 @@
 #include "video.hpp"
 using namespace Video;
 
-void Videos::star(bool op, unsigned weight, unsigned height){
-    std::string url = "0";
-    if (!op) url = isPhone() ? PHONE : WIFI;
+unsigned Video::count = 0;
 
-    cap.open(url, cv::CAP_FFMPEG);
+void Videos::star(bool op, unsigned weight, unsigned height){
+/***************************************************************************************
+*               时间：2025-4-26
+*               
+*               实现：连接摄像头，初始化last_frame
+*
+*               错误：暂无
+*               
+*               日志：暂无
+*
+***************************************************************************************/
+    if (!op) cap.open(std::string{ isPhone() ? PHONE : WIFI });
+    else cap.open(0);
     cap.read(last_frame);
-    cv::resize(last_frame, last_frame, cv::Size(WEIGHT, HEIGHT));
+    cv::resize(last_frame, last_frame, cv::Size(weight, height));
 }
 
-void Videos::show(unsigned weight, unsigned height) {
-    cv::namedWindow("swap face");
-    while(true) {
-        if (count % 3 == 0) {
-            if (!frame_queue.empty()) {
-                last_frame = frame_queue.front();
-                frame_queue.pop();
-            } else {
-                cap.read(last_frame);
-            }
-
-            cv::imshow("swap face", last_frame);
+void Videos::capture_loop() {
+    cv::Mat frame;
+    while (is_running) {
+        if (cap.read(frame)) {
+            std::lock_guard<std::mutex> lock(frame_mutex);
+            last_frame = frame;
+            is_frame_ready = true;
+            frame_ready.notify_one();
         }
+        // 控制帧率
+        //std::this_thread::sleep_for(std::chrono::milliseconds(1000 / target_fps));
+    }
+}
 
+void Videos::display_loop(unsigned weight, unsigned height) {
+    cv::namedWindow("swap face", cv::WINDOW_FREERATIO);
+    cv::resizeWindow("swap face", weight, height);
+    
+    while (is_running) {
+        {
+            std::unique_lock<std::mutex> lock(frame_mutex);
+            frame_ready.wait(lock, [this] { return is_frame_ready || !is_running; });
+            
+            if (!is_running) break;
+            
+            cv::imshow("swap face", last_frame);
+            is_frame_ready = false;
+        }
+        
         if (cv::waitKey(1) == 27) {
-            close();
+            is_running = false;
             break;
         }
     }
 }
 
+void Videos::show(unsigned weight, unsigned height) {
+/***************************************************************************************
+*               时间：2025-4-26
+*               
+*               实现：显示视频
+*
+*               错误：暂无
+*               
+*               日志：连接手机摄像头非常卡顿，已使用多线程修复
+*
+***************************************************************************************/
+    is_running = true;
+    is_frame_ready = false;
+    
+    // 启动捕获线程
+    capture_thread = std::thread(&Videos::capture_loop, this);
+    
+    // 启动显示线程
+    display_thread = std::thread(&Videos::display_loop, this, weight, height);
+    
+    // 等待显示线程结束
+    display_thread.join();
+    
+    // 停止捕获线程
+    is_running = false;
+    capture_thread.join();
+    
+    close();
+}
+
 void Videos::close() {
+/***************************************************************************************
+*               时间：2025-4-26
+*               
+*               实现：关闭所有流
+*
+*               错误：暂无
+*               
+*               日志：暂无
+*
+***************************************************************************************/
+    is_running = false;
+    if (capture_thread.joinable()) capture_thread.join();
+    if (display_thread.joinable()) display_thread.join();
     cap.release();
     last_frame.release();
     while(!frame_queue.empty()) frame_queue.pop();
 }
 
 bool Videos::isPhone(){
+/***************************************************************************************
+*               时间：2025-4-26
+*               
+*               实现：判断是否连接手机
+*
+*               错误：暂无
+*               
+*               日志：暂无
+*
+***************************************************************************************/
     HANDLE hClient = NULL;
     DWORD dwVersion = 0;
     bool ret = true;
--- video.hpp
+++ video.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <opencv2/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -5,15 +6,17 @@
 #include <iostream>
 #include <windows.h>
 #include <wlanapi.h>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
 #pragma comment(lib, "wlanapi.lib")
-
-#define WLAN_API_VERSION 2
 /***************************************************************************************
 *               时间：2025-4-25
 *               
 *               实现：摄像头获取
 *
-*               错误：
+*               错误：暂无
 *               
 *               日志：暂未完成
 *
@@ -23,10 +26,9 @@ namespace Video{
     /***********************************命名空间变量*************************************/
     const unsigned WEIGHT = 1280u;
     const unsigned HEIGHT = 800u;
-    const std::string_view PHONE = "";
-    const std::string_view WIFI = "";
-    unsigned count = 0;
-
+    const std::string_view PHONE = "http://10.242.10.156:8080/videofeed?fflags=nobuffer&flags=low_delay";
+    const std::string_view WIFI = "http://192.168.1.103:8080/videofeed?fflags=nobuffer&flags=low_delay";
+    extern unsigned count;
 
     class Videos{
     /***********************************成员变量*************************************/
@@ -34,6 +36,14 @@ namespace Video{
         cv::Mat last_frame;
         std::queue<cv::Mat> frame_queue;
         cv::VideoCapture cap;
+    private:
+        std::thread capture_thread;
+        std::thread display_thread;
+        std::mutex frame_mutex;
+        std::condition_variable frame_ready;
+        std::atomic<bool> is_running{false};
+        std::atomic<bool> is_frame_ready{false};
+        unsigned target_fps = 30;
     /***********************************成员函数*************************************/
     public:
         void star(bool op = false, unsigned weight = WEIGHT, unsigned height = HEIGHT);
@@ -41,5 +51,7 @@ namespace Video{
     private:
         void close();
         bool isPhone();
+        void capture_loop();
+        void display_loop(unsigned weight, unsigned height);
     };
 }
